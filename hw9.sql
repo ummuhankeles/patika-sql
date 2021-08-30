047cc7cff82884a05be0f8e492ae0d44b7db247a
Ümmühan Keleş <ummuhankeles4@gmail.com>
2021-08-30 19:17:34 +0300

    completed hw9

---
+++ hw9.sql
@@ -0,0 +1,17 @@
+-- query 1
+-- city tablosu ile country tablosunda bulunan şehir (city) ve ülke (country) isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+SELECT city, country FROM city
+INNER JOIN 	country ON city.country_id = country.country_id;
+
+-- query 2
+-- customer tablosu ile payment tablosunda bulunan payment_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+SELECT payment.payment_id, customer.first_name, customer.last_name FROM payment
+INNER JOIN customer ON payment.customer_id = payment.customer_id;
+
+-- query 3
+-- customer tablosu ile rental tablosunda bulunan rental_id ile customer tablosundaki first_name ve last_name isimlerini birlikte görebileceğimiz INNER JOIN sorgusunu yazınız.
+
+SELECT rental.rental_id, customer.first_name, customer.last_name FROM rental
+INNER JOIN customer ON rental.customer_id = customer.customer_id;
